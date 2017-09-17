af23f37868ab0f5b88ee16682197d45f361e75e5
Brian Jensen <31685529+jensen-brian@users.noreply.github.com>
2017-09-17 00:18:15 -0700

    More practice take two.

--- src/com/practice/code/BrainTeasers.java
+++ src/com/practice/code/BrainTeasers.java
@@ -1,6 +1,9 @@
 package com.practice.code;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -8,6 +11,10 @@ import java.util.Map;
  * 
  * @author Brian Jensen
  * Date: 09/15/2017
+ */
+/**
+ * @author brian.jensen
+ *
  */
 public class BrainTeasers {
 	public static String FIZZ = "fizz";
@@ -85,5 +92,91 @@ public class BrainTeasers {
 		}
 		System.out.println(String.format("%s == %s", word, reverse.toString()));
 		return word.equals(reverse.toString());
+	}
+
+	/**
+	 * Rotate an array of n elements to the right by k steps.
+	 * 
+	 * @param nums The array to rotate.
+	 * @param k Rotate to the right k steps.
+	 * @return
+	 */
+	public static int[] rotate(int[] nums, int k) {
+		// do not process if the array is null or k < 1
+		if (nums == null || k < 1) return null;
+		
+		System.out.println("Input: " + Arrays.toString(nums));
+		System.out.println("Rotate on: " + k);
+		
+		// create the indexer from the back of the array
+		int indexer = (nums.length - k);
+		
+		int[] result = new int[nums.length];
+		for (int i = 0; i < nums.length; i++) {
+			indexer = (indexer < nums.length) ? indexer : 0;
+			int val = nums[indexer++];
+			// put the new value in the array
+			result[i] = val;
+		}
+		
+		System.out.println("Output: " + Arrays.toString(result));
+		return result;
+	}
+
+	/**
+	 * Given an array and a value, remove all instances of that value in place and return the new length.
+	 * 
+	 * @param nums The array to remove val from.
+	 * @param val The value to remove from the array.
+	 * @return The number of matching values in the array.
+	 */
+	public static int removeElement(int[] nums, int val) {
+		System.out.println("Start: " + Arrays.toString(nums));
+		
+		int count = 0;
+		int moveIndex = 0;
+		for (int i = 0; i < nums.length; i++) {
+			int number = nums[i];
+			nums[i] = -1; // remove
+			
+			if (number != val) {
+				// count where it does not match
+				count++;
+				
+				nums[moveIndex] = number;
+				moveIndex++;
+			}
+		}
+		
+		System.out.println("End:   " + Arrays.toString(nums));
+		return count;
+	}
+
+	/**
+	 * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+	 * 
+	 * @param nums Array of numbers to add up two to equal the target.
+	 * @param target The target to add two numbers to equal.
+	 * @return An Array of the two numbers that add to the target.
+	 */
+	public static int[] twoSum(int[] nums, int target) {
+		// must have two number to add and cannot be negative target
+		if (nums == null || nums.length < 2 || target < 0) return null;
+
+		System.out.println("Start: " + Arrays.toString(nums));
+		
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 1; j < nums.length; j++) {
+				int one = nums[i];
+				int two = nums[j];
+				if ((one + two) == target) {
+					int[] found = {one, two};
+					System.out.println("Found: " + Arrays.toString(found));
+					return found;
+				}
+			}
+		}
+
+		return null;
 	}	
 }
